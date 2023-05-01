647cd0e76894c0dfc3be75584613035b6962c16e
Adesh0318 <130365061+Adesh0318@users.noreply.github.com>
2023-05-01 13:01:59 +0000

    container config done

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 412154
+let accountEmail = "adesh.malik@webority.com"
+var accountPassword = "12345Adesh"
+accountCity = "jaipur"
+
+// accountId = "4546"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
