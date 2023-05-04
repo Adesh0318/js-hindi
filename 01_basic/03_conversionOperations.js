a787f7c3cc643f9413caf107e97fe05981700b57
Adesh0318 <130365061+Adesh0318@users.noreply.github.com>
2023-05-04 13:31:54 +0000

    operations

--- 01_basic/03_conversionOperations.js
+++ 01_basic/03_conversionOperations.js
@@ -1,22 +1,62 @@
-let score = "33aaa"
+// let score = "33aaa"
 
-// console.log(typeof score);
-// console.log(typeof(score));
+// // console.log(typeof score);
+// // console.log(typeof(score));
 
-let valueInNumber = Number(score)
-// console.log(typeof valueInNumber);
-// console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// // console.log(typeof valueInNumber);
+// // console.log(valueInNumber);
 
-// "33" => 33
-// "33aaa" => NaN
-// true => 1; false => 0;
+// // "33" => 33
+// // "33aaa" => NaN
+// // true => 1; false => 0;
 
-let isLoggedIn="1"
+// let isLoggedIn="1"
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 
-// 1=> true; 0=> false;
-// "" => false
-// "hitesh" => true.
+// // 1=> true; 0=> false;
+// // "" => false
+// // "hitesh" => true.
+
+
+// *****************///
+
+let value = 3
+let negValue = -value
+// console.log (negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Adesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+//  console.log("1" + 2);
+//  console.log(1 + "2");
+//  console.log("1" + 2 + 2);
+//  console.log(1 + 2 + "2");
+
+//  console.log( (3 + 4) * 5 % 3);
+
+//  console.log(+true);
+//  console.log(+""); /* there is no sense;*/
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 +2 ;
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+// here we can use the prefix or postfix operator;
