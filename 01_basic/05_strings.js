831b7db2844a7dcde3e2e0811ec41dc93e1327ac
Adesh0318 <130365061+Adesh0318@users.noreply.github.com>
2023-05-12 12:34:27 +0000

    gamename

---
+++ 01_basic/05_strings.js
@@ -0,0 +1,37 @@
+ const name = "adesh"
+ const repoCount = 50
+
+ //old method to write that //console.log( name + repoCount + "value");
+
+ console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+//  const gameName = new String('adeshMalik')
+// //  console.log(gameName)
+// //  console.log(gameName[0]);
+
+// // console.log(gameName.length);
+// // console.log(gameName.toUpperCase());
+// // console.log(gameName.charAt(2));
+// // console.log(gameName.indexOf('e'));
+
+const gameName = new String('adesh-wb')
+
+// const newString = gameName.substring(0, 4) 
+// console.log(newString);
+
+// const anotherString = gameName.slice(-7, 4)
+// console.log(anotherString);
+
+// const newStringOne = "    adesh    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+//Replace 
+
+
+const url = "https://adeshmalik.com/adesh%21choudhary"
+
+console.log(url.replace('%21','_'));
+
+console.log(gameName.split('-'));
+
