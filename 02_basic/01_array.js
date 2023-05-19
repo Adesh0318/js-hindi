33b57c56d4f51f7cb94af3b9c0020af540cca8cf
Adesh0318 <130365061+Adesh0318@users.noreply.github.com>
2023-05-19 07:14:06 +0000

    aray

--- 02_basic/01_array.js
+++ 02_basic/01_array.js
@@ -3,22 +3,22 @@
 const myArr = [0, 1, 2, 3, 4, 5]
 const myHeors = ["shaktiman", "naagraj"]
 
-const myArr2 = new Array(1, 2, 3, 4)
-// console.log(myArr[1]);
+const myArr2 = new Array(1, 2, 5, 4)
+console.log(myArr2[2]);
 
 // Array methods
 
-// myArr.push(6)
-// myArr.push(7)
-// myArr.pop()
+// myArr.push(6) add the value in array
+// myArr.push(7) 
+// myArr.pop()delete the last value
 
-// myArr.unshift(9)
-// myArr.shift()
+// myArr.unshift(9) add the value in first position of array
+// myArr.shift() delete the first value of array
 
-// console.log(myArr.includes(9));
-// console.log(myArr.indexOf(3));
+// console.log(myArr.includes(9)); Th ask the question in array is the 9 available or not.
+// console.log(myArr.indexOf(3)); to find the index in array
 
-// const newArr = myArr.join()
+// const newArr = myArr.join() to convert all the array value in to the string
 
 // console.log(myArr);
 // console.log( newArr);
@@ -26,14 +26,14 @@ const myArr2 = new Array(1, 2, 3, 4)
 
 // slice, splice
 
-console.log("A ", myArr);
+// console.log("A ", myArr);
 
-const myn1 = myArr.slice(1, 3)
+// const myn1 = myArr.slice(1, 3)
 
-console.log(myn1);
-console.log("B ", myArr);
+// console.log(myn1);
+// console.log("B ", myArr);
 
 
-const myn2 = myArr.splice(1, 3)
-console.log("C ", myArr);
-console.log(myn2);
+// const myn2 = myArr.splice(1, 3)
+// console.log("C ", myArr);
+// console.log(myn2);
